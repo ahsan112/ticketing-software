ce27d5a79db61213c7ed2ff446f8233c16f2e0ae
Ahsan Mirza <Ahsan@Ahsans-MacBook-Pro.local>
2021-11-08 10:43:12 +0000

    made role middleware + hid register route behind the admin role

---
+++ src/app/Http/Middleware/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, String $role)
+    {
+        abort_if($request->user()->role() != $role, 403);
+
+        return $next($request);
+    }
+}
