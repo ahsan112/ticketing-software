95d73d3758057a74a1d85dd1591aaf63d98beefd
Ahsan Mirza <Ahsan@Ahsans-MacBook-Pro.local>
2021-11-08 11:18:07 +0000

    added ticket controller + route

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::view('/', 'dashboard')->name('dashboard');
+
+    Route::resource('tickets', TicketController::class);
 });
 
 require __DIR__.'/auth.php';
